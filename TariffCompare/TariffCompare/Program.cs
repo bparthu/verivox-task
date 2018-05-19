917705e0364858c80549271d8e9ae65b7b3ebe1a
b.paarthu <b.paarthu@gmail.com>
2018-05-19 18:38:11 +0200

    created new visual studio project and added gitignore

---
+++ TariffCompare/TariffCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TariffCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
